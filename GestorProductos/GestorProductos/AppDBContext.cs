a629c0fc9f051bca7daf8222bcfa7f1b23052f33
ErickxSebastianMora <esmora2@espe.edu.ec>
2025-01-13 11:53:16 -0500

    context

---
+++ GestorProductos/GestorProductos/AppDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace GestorProductos
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Producto> Productos { get; set; }
+
+        public class Producto
+        {
+            public int Id { get; set; }
+            public string? Nombre { get; set; }
+            public string? Descripcion { get; set; }
+            public decimal Precio { get; set; }
+            public int Stock { get; set; }
+        }
+    }
+    
+    
+}
---
+++ GestorProductos/GestorProductos/Controllers/ProductosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorProductos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productosController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        public string password;
+
+        public productosController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        { 
+            return Ok(await _dbContext.Productos.ToListAsync());
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
